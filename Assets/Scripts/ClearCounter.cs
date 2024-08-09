46ce5b3eb9af6af117a581e4196cf4c2e32f704f
itodobien <itodobien@gmail.com>
2024-08-08 22:20:56 -0700

    Object Placement
    Added logic to allow objects to be placed on countertops through interact. Added testing to enable movement of object from one countertop to another if counter top is empty.

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,12 +1,64 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+
+    [SerializeField] private KitchenObjectScriptObj kitchenObjectSciptObj;
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
+    private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+        {
+            if (kitchenObject != null)
+            {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
     public void Interact()
     {
-        Debug.Log("Interact!");
+        if (kitchenObject == null)
+        {
+            // Spawn a tomato at the counter top point
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSciptObj.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else
+        {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }        
+       
     }
-    
+    public Transform GetkitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
 }
